c5b4ad6af50f5304df421295a27c591956567294
Jason Main <jason_main@abercrombie.com>
2016-06-09 21:51:47 -0400

    added buttons to add and remove wish list items

--- js/wishList.js
+++ js/wishList.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+  $( ".selectable" ).selectable();
 
+  $("#add").on("click", function() {
+      $(".wishList").append("<li class='ui-widget-content'>item </li>");
+  })
+
+  $("#remove").on("click", function() {
+    $(".ui-selected").remove();
+  });
 });
